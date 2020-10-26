86118c49dfd3b4a9ba89e0996bc079e7a41ed6ad
Carlos Rafael Giani <crg7475@mailbox.org>
2020-10-26 11:30:25 +0100

    comboctl-base: Derive BluetoothException from ComboIOException
    Bluetooth activity is arguably IO activity. So, inheriting from
    ComboIOException makes more sense.
    
    Signed-off-by: Carlos Rafael Giani <crg7475@mailbox.org>

--- comboctl/src/main/kotlin/info/nightscout/comboctl/base/BluetoothException.kt
+++ comboctl/src/main/kotlin/info/nightscout/comboctl/base/BluetoothException.kt
@@ -6,7 +6,7 @@ package info.nightscout.comboctl.base
  * @param message The detail message.
  * @param cause Throwable that further describes the cause of the exception.
  */
-open class BluetoothException(message: String?, cause: Throwable?) : ComboException(message, cause) {
+open class BluetoothException(message: String?, cause: Throwable?) : ComboIOException(message, cause) {
     constructor(message: String) : this(message, null)
     constructor(cause: Throwable) : this(null, cause)
 }
