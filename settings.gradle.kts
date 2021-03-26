9ca1b3868b7b18d9e786703ce2b751cb62aee799
AdrianLxM <adrian.m@inbox.com>
2021-03-26 21:04:59 +0100

    fix android manifest and work around gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,5 @@
 rootProject.name = "ComboCtl"
 include(":comboctl")
-include(":comboctl:src:linuxBlueZCpp")
-include(":comboctl:src:linuxBlueZCpp:external:fmtlib")
-include(":comboctl:src:jvmMain:cpp:linuxBlueZCppJNI")
 include(":devtools:common")
 include(":devtools:dump_packets")
 include(":devtools:bluez_cli")
@@ -12,5 +9,8 @@ val env = System.getProperties()
 if (env["idea.platform.prefix"] != "AndroidStudio") {
     logger.lifecycle("Not building with Android Studio; enabling javafxApp")
     include(":javafxApp")
+    include(":comboctl:src:linuxBlueZCpp")
+    include(":comboctl:src:linuxBlueZCpp:external:fmtlib")
+    include(":comboctl:src:jvmMain:cpp:linuxBlueZCppJNI")
 } else
     logger.lifecycle("Building with Android Studio; disabling javafxApp")
