295ee33aaae2507c503e15000e211a23c84f7553
Carlos Rafael Giani <crg7475@mailbox.org>
2021-03-26 14:38:00 +0100

    javafxApp: Enable build only when not building with Android Studio
    Signed-off-by: Carlos Rafael Giani <crg7475@mailbox.org>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,4 +6,11 @@ include(":comboctl:src:jvmMain:cpp:linuxBlueZCppJNI")
 include(":devtools:common")
 include(":devtools:dump_packets")
 include(":devtools:bluez_cli")
-include(":javafxApp")
+
+val env = System.getProperties()
+
+if (env["idea.platform.prefix"] != "AndroidStudio") {
+    logger.lifecycle("Not building with Android Studio; enabling javafxApp")
+    include(":javafxApp")
+} else
+    logger.lifecycle("Building with Android Studio; disabling javafxApp")
