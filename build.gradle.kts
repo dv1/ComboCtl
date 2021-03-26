e2f5a65ab66b100bcff599dbaff99c1e60d666b8
Carlos Rafael Giani <crg7475@mailbox.org>
2021-03-26 15:10:56 +0100

    Update Android Gradle plugin to version 4.1.3
    Signed-off-by: Carlos Rafael Giani <crg7475@mailbox.org>

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ buildscript {
     }
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.31")
-        classpath("com.android.tools.build:gradle:4.1.0")
+        classpath("com.android.tools.build:gradle:4.1.3")
     }
 }
 
