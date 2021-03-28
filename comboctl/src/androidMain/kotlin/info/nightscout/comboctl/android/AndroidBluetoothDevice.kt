708a6476eed3edce489f4bc5cea3c6bd86e2ec93
Carlos Rafael Giani <crg7475@mailbox.org>
2021-03-28 11:57:27 +0200

    comboctl-android: Finish Android Bluetooth code
    Signed-off-by: Carlos Rafael Giani <crg7475@mailbox.org>

--- comboctl/src/androidMain/kotlin/info/nightscout/comboctl/android/AndroidBluetoothDevice.kt
+++ comboctl/src/androidMain/kotlin/info/nightscout/comboctl/android/AndroidBluetoothDevice.kt
@@ -13,12 +13,20 @@ import info.nightscout.comboctl.base.Logger
 import java.io.IOException
 import java.io.InputStream
 import java.io.OutputStream
+import java.util.Locale
 
 private val logger = Logger.get("AndroidBluetoothDevice")
 
+/**
+ * Class representing a Bluetooth device accessible through Android's Bluetooth API.
+ *
+ * Users typically do not instantiate this directly. Instead,
+ * [AndroidBluetoothInterface]'s implementation of [BluetoothInterface.getDevice]
+ * instantiates and returns this (as a [BluetoothDevice]).
+ */
 class AndroidBluetoothDevice(
     bluetoothInterface: BluetoothInterface,
-    private val osBluetoothAdapter: SystemBluetoothAdapter,
+    private val systemBluetoothAdapter: SystemBluetoothAdapter,
     override val address: BluetoothAddress
 ) : BluetoothDevice(bluetoothInterface) {
     private var systemBluetoothSocket: SystemBluetoothSocket? = null
@@ -35,31 +43,55 @@ class AndroidBluetoothDevice(
 
         lateinit var device: SystemBluetoothDevice
         try {
-            device = osBluetoothAdapter.getRemoteDevice(address.toString())
+            // Use toUpperCase() since Android expects the A-F hex
+            // digits in the Bluetooth address string to be uppercase
+            // (lowercase ones are considered invalid and cause an
+            // exception to be thrown).
+            device = systemBluetoothAdapter.getRemoteDevice(address.toString().toUpperCase(Locale.ROOT))
         } catch (e: IllegalArgumentException) {
             throw BluetoothException("Bluetooth address $address is invalid according to Android", e)
         }
 
         logger(LogLevel.DEBUG) { "Attempting to connect RFCOMM socket to device" }
 
+        // The Combo communicates over RFCOMM using the SDP Serial Port Profile.
+        // We use an insecure socket, which means that it lacks an authenticated
+        // link key. This is done because the Combo does not use this feature.
         try {
             systemBluetoothSocket = device.createInsecureRfcommSocketToServiceRecord(Constants.sdpSerialPortUUID)
         } catch (e: IOException) {
             throw BluetoothException("Could not connect RFCOMM socket to device with address $address", e)
         }
 
+        // connect() must be explicitely called. Just creating the socket via
+        // createInsecureRfcommSocketToServiceRecord() does not implicitely
+        // establish the connection. This is important to keep in mind, since
+        // otherwise, the calls below get input and output streams that seem
+        // to be OK until their read/write functions are actually used. At
+        // that point, very confusing NullPointerExceptions are thrown from
+        // seemingly nowhere. These NPEs happen because *inside* the streams
+        // there are internal Input/OutputStreams, and *these* are set to null
+        // if the connection wasn't established. See also:
+        // https://stackoverflow.com/questions/24267671/inputstream-read-causes-nullpointerexception-after-having-checked-inputstream#comment37491136_24267671
+        // and: https://stackoverflow.com/a/24269255/560774
         try {
-            inputStream = systemBluetoothSocket!!.getInputStream()
+            systemBluetoothSocket!!.connect()
         } catch (e: IOException) {
-            disconnectImpl()
-            throw BluetoothException("Could not get input stream to device with address $address", e)
+            throw BluetoothException("Could not connect RFCOMM socket to device with address $address", e)
         }
 
         try {
-            outputStream = systemBluetoothSocket!!.getOutputStream()
+            inputStream = systemBluetoothSocket!!.inputStream
         } catch (e: IOException) {
             disconnectImpl()
-            throw BluetoothException("Could not get output stream to device with address $address", e)
+            throw ComboIOException("Could not get input stream to device with address $address", e)
+        }
+
+        try {
+            outputStream = systemBluetoothSocket!!.outputStream
+        } catch (e: IOException) {
+            disconnectImpl()
+            throw ComboIOException("Could not get output stream to device with address $address", e)
         }
 
         logger(LogLevel.INFO) { "RFCOMM connection with device with address $address established" }
--- comboctl/src/androidMain/kotlin/info/nightscout/comboctl/android/AndroidBluetoothInterface.kt
+++ comboctl/src/androidMain/kotlin/info/nightscout/comboctl/android/AndroidBluetoothInterface.kt
@@ -18,11 +18,23 @@ import java.util.concurrent.locks.ReentrantLock
 
 private val logger = Logger.get("AndroidBluetoothInterface")
 
+/**
+ * Class for accessing Bluetooth functionality on Android.
+ *
+ * This needs an Android [Context] that is always present for
+ * the duration of the app's existence. It is not recommended
+ * to use the context from an [Activity], since such a context
+ * may go away if the user turns the screen for example. If
+ * the context goes away, and discovery is ongoing, then that
+ * discovery prematurely ends. The context of an [Application]
+ * instance is an ideal choice.
+ */
 class AndroidBluetoothInterface(private val androidContext: Context) : BluetoothInterface {
     private val bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
     private var rfcommServerSocket: SystemBluetoothServerSocket? = null
     private var discoveryStarted = false
     private var discoveryBroadcastReceiver: BroadcastReceiver? = null
+
     // Note that this contains ALL paired/bonded devices,
     // not just the ones that pass the deviceFilter. This
     // is important in case the filter is changed sometime
@@ -30,14 +42,25 @@ class AndroidBluetoothInterface(private val androidContext: Context) : Bluetooth
     // return an incomplete list.getPairedDeviceAddresses()
     // has to apply the filter manually.
     private val pairedDeviceAddresses = mutableSetOf<BluetoothAddress>()
+
+    // This is necessary, since the BroadcastReceivers always
+    // run in the UI thread, while access to the pairedDeviceAddresses
+    // can be requested from other threads.
     private val deviceAddressLock = ReentrantLock()
+
     private var unpairedDevicesBroadcastReceiver: BroadcastReceiver? = null
+
+    private val previouslyDiscoveredDevices = mutableSetOf<BluetoothAddress>()
+
+    private var discoveryStopped: (reason: BluetoothInterface.DiscoveryStoppedReason) -> Unit = { }
 
     override var onDeviceUnpaired: (deviceAddress: BluetoothAddress) -> Unit = { }
 
     override var deviceFilter: (deviceAddress: BluetoothAddress) -> Boolean = { true }
 
     fun setup() {
+        previouslyDiscoveredDevices.clear()
+
         val bondedDevices = bluetoothAdapter.getBondedDevices()
 
         logger(LogLevel.DEBUG) { "Found ${bondedDevices.size} bonded Bluetooth device(s)" }
@@ -88,17 +111,27 @@ class AndroidBluetoothInterface(private val androidContext: Context) : Bluetooth
         sdpServiceProvider: String,
         sdpServiceDescription: String,
         btPairingPin: String,
+        discoveryDuration: Int,
+        discoveryStopped: (reason: BluetoothInterface.DiscoveryStoppedReason) -> Unit,
         foundNewPairedDevice: (deviceAddress: BluetoothAddress) -> Unit
     ) {
         if (discoveryStarted)
             throw IllegalStateException("Discovery already started")
 
+        // The Combo communicates over RFCOMM using the SDP Serial Port Profile.
+        // We use an insecure socket, which means that it lacks an authenticated
+        // link key. This is done because the Combo does not use this feature.
+        //
+        // TODO: Can Android RFCOMM SDP service records be given custom
+        // sdpServiceProvider and sdpServiceDescription values? (This is not
+        // necessary for correct function, just a detail for sake of completeness.)
         logger(LogLevel.DEBUG) { "Setting up RFCOMM listener socket" }
         rfcommServerSocket = bluetoothAdapter.listenUsingInsecureRfcommWithServiceRecord(
             sdpServiceName,
             Constants.sdpSerialPortUUID
         )
-        // NOTE: sdpServiceProvider and sdpServiceDescription are not used here.
+
+        this.discoveryStopped = discoveryStopped
 
         logger(LogLevel.DEBUG) {
             "Registering receiver for getting notifications about pairing requests and connected devices"
@@ -107,6 +140,7 @@ class AndroidBluetoothInterface(private val androidContext: Context) : Bluetooth
         val intentFilter = IntentFilter()
         intentFilter.addAction(SystemBluetoothDevice.ACTION_ACL_CONNECTED)
         intentFilter.addAction(SystemBluetoothDevice.ACTION_PAIRING_REQUEST)
+        intentFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED)
 
         discoveryBroadcastReceiver = object : BroadcastReceiver() {
             override fun onReceive(context: Context, intent: Intent) {
@@ -115,6 +149,7 @@ class AndroidBluetoothInterface(private val androidContext: Context) : Bluetooth
                 when (intent.getAction()) {
                     SystemBluetoothDevice.ACTION_ACL_CONNECTED -> onAclConnected(intent, foundNewPairedDevice)
                     SystemBluetoothDevice.ACTION_PAIRING_REQUEST -> onPairingRequest(intent, btPairingPin)
+                    BluetoothAdapter.ACTION_DISCOVERY_FINISHED -> onDiscoveryFinished()
                     else -> Unit
                 }
             }
@@ -124,13 +159,13 @@ class AndroidBluetoothInterface(private val androidContext: Context) : Bluetooth
 
         logger(LogLevel.DEBUG) { "Starting activity for making this Android device discoverable" }
 
-        // TODO: Currently, the ComboCtl API does not support time-limited
-        // discovery. Either, add this to the API, or try to work around
-        // this Android limitation, for example by detecting the end of
-        // the discoverable mode & re-requesting it then.
         val discoverableIntent = Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE).apply {
-            putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300)
+            putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, discoveryDuration)
             putExtra(BluetoothAdapter.EXTRA_SCAN_MODE, BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE)
+            // Necessary since we want to be able to start scans from any
+            // context, not just from activities. In fact, starting scans
+            // from activities would be a back pick, since they can  can
+            // go away at any moment, taking the ongoing scan with them.
             flags = flags or Intent.FLAG_ACTIVITY_NEW_TASK
         }
         androidContext.startActivity(discoverableIntent)
@@ -167,6 +202,8 @@ class AndroidBluetoothInterface(private val androidContext: Context) : Bluetooth
     }
 
     override fun unpairDevice(deviceAddress: BluetoothAddress) {
+        // NOTE: Unfortunately, Android has no public removeBond() functionality,
+        // so we cannot do anything here.
     }
 
     override fun getDevice(deviceAddress: BluetoothAddress): BluetoothDevice =
@@ -184,6 +221,14 @@ class AndroidBluetoothInterface(private val androidContext: Context) : Bluetooth
         }
 
     private fun onAclConnected(intent: Intent, foundNewPairedDevice: (deviceAddress: BluetoothAddress) -> Unit) {
+        // Sanity check in case we get this notification for the
+        // device already and need to avoid duplicate processing.
+        if (intent.getStringExtra("address") != null)
+            return
+
+        // Sanity check to make sure we can actually get
+        // a Bluetooth device out of the intent. Otherwise,
+        // we have to wait for the next notification.
         val androidBtDevice = intent.getParcelableExtra<SystemBluetoothDevice>(SystemBluetoothDevice.EXTRA_DEVICE)
         if (androidBtDevice == null) {
             logger(LogLevel.DEBUG) { "Ignoring ACL_CONNECTED intent that has no Bluetooth device" }
@@ -191,7 +236,9 @@ class AndroidBluetoothInterface(private val androidContext: Context) : Bluetooth
         }
 
         val androidBtAddressString = androidBtDevice.getAddress()
-        // intent.putExtra("address", androidBtAddressString)
+        // This effectively marks the device as "already processed"
+        // (see the getStringExtra() call above).
+        intent.putExtra("address", androidBtAddressString)
 
         logger(LogLevel.DEBUG) { "ACL_CONNECTED intent has Bluetooth device with address $androidBtAddressString" }
 
@@ -205,6 +252,47 @@ class AndroidBluetoothInterface(private val androidContext: Context) : Bluetooth
             return
         }
 
+        // During discovery, the ACTION_ACL_CONNECTED action apparently
+        // is notified at least *twice*. And, the device that is present
+        // in the intent may not be the same device there was during
+        // the first notification (= the parcelableExtra EXTRA_DEVICE
+        // seen above). It turns out that only the *second EXTRA_DEVICE
+        // can actually be used (otherwise pairing fails). The reason
+        // for this is unknown, but seems to be caused by bugs in the
+        // Fluoride (aka BlueDroid) Bluetooth stack.
+        // To circumvent this, we don't do anything the first time,
+        // but remember the device's Bluetooth address. Only when we
+        // see that address again do we actually proceed with announcing
+        // the device as having been discovered.
+        // NOTE: This is different from the getStringExtra() check
+        // above. That one checks if the *same* Android Bluetooth device
+        // instance was already processed. This check here instead
+        // verifies if we have seen the same Bluetooth address on
+        // *different* Android Bluetooth device instances.
+        // TODO: Test how AndroidBluetoothInterface behaves if the
+        // device is unpaired while discovery  is ongoing (manually by
+        // the user for example). In theory, this should be handled
+        // properly by the onBondStateChanged function below.
+        // TODO: This check may not be necessary on all Android
+        // devices. On some, it seems to also work if we use the
+        // first offered BluetoothDevice.
+        if (comboctlBtAddress !in previouslyDiscoveredDevices) {
+            previouslyDiscoveredDevices.add(comboctlBtAddress)
+            logger(LogLevel.DEBUG) {
+                "Device with address $comboctlBtAddress discovered for the first time; " +
+                "need to \"discover\" it again to be able to announce its discovery"
+            }
+            return
+        } else {
+            logger(LogLevel.DEBUG) {
+                "Device with address $comboctlBtAddress discovered for the second time; " +
+                "announcing it as discovered"
+            }
+        }
+
+        // Always adding the device to the paired addresses even
+        // if the deviceFilter() below returns false. See the
+        // pairedDeviceAddresses comments above for more.
         try {
             deviceAddressLock.lock()
             pairedDeviceAddresses.add(comboctlBtAddress)
@@ -215,6 +303,10 @@ class AndroidBluetoothInterface(private val androidContext: Context) : Bluetooth
         logger(LogLevel.INFO) { "Got device with address $androidBtAddressString" }
 
         try {
+            // Apply device filter before announcing a newly
+            // discovered device, just as the ComboCtl
+            // BluetoothInterface.startDiscovery()
+            // documentation requires.
             if (deviceFilter(comboctlBtAddress))
                 foundNewPairedDevice(comboctlBtAddress)
         } catch (e: Exception) {
@@ -223,6 +315,12 @@ class AndroidBluetoothInterface(private val androidContext: Context) : Bluetooth
     }
 
     private fun onBondStateChanged(intent: Intent) {
+        // Here, we handle the case where a previously paired
+        // device just got unpaired. The caller needs to know
+        // about this to check if said device was a Combo.
+        // If so, the caller may have to update states like
+        // the pump state store accordingly.
+
         val androidBtDevice = intent.getParcelableExtra<SystemBluetoothDevice>(SystemBluetoothDevice.EXTRA_DEVICE)
         if (androidBtDevice == null) {
             logger(LogLevel.DEBUG) { "Ignoring BOND_STATE_CHANGED intent that has no Bluetooth device" }
@@ -230,7 +328,6 @@ class AndroidBluetoothInterface(private val androidContext: Context) : Bluetooth
         }
 
         val androidBtAddressString = androidBtDevice.getAddress()
-        // intent.putExtra("address", androidBtAddressString)
 
         logger(LogLevel.DEBUG) { "PAIRING_REQUEST intent has Bluetooth device with address $androidBtAddressString" }
 
@@ -247,9 +344,16 @@ class AndroidBluetoothInterface(private val androidContext: Context) : Bluetooth
         val previousBondState = intent.getIntExtra(SystemBluetoothDevice.EXTRA_PREVIOUS_BOND_STATE, SystemBluetoothDevice.ERROR)
         val currentBondState = intent.getIntExtra(SystemBluetoothDevice.EXTRA_BOND_STATE, SystemBluetoothDevice.ERROR)
 
+        // An unpaired device is characterized by a state change
+        // from non-NONE to NONE. Filter out all other state changes.
         if (!((currentBondState == SystemBluetoothDevice.BOND_NONE) && (previousBondState != SystemBluetoothDevice.BOND_NONE)))
             return
 
+        previouslyDiscoveredDevices.remove(comboctlBtAddress)
+
+        // Always removing the device from the paired addresses
+        // event if the deviceFilter() below returns false. See
+        // the pairedDeviceAddresses comments above for more.
         try {
             deviceAddressLock.lock()
             pairedDeviceAddresses.remove(comboctlBtAddress)
@@ -258,6 +362,10 @@ class AndroidBluetoothInterface(private val androidContext: Context) : Bluetooth
             deviceAddressLock.unlock()
         }
 
+        // Apply device filter before announcing an
+        // unpaired device, just as the ComboCtl
+        // BluetoothInterface.startDiscovery()
+        // documentation requires.
         try {
             if (deviceFilter(comboctlBtAddress))
                 onDeviceUnpaired(comboctlBtAddress)
@@ -274,7 +382,6 @@ class AndroidBluetoothInterface(private val androidContext: Context) : Bluetooth
         }
 
         val androidBtAddressString = androidBtDevice.getAddress()
-        // intent.putExtra("address", androidBtAddressString)
 
         logger(LogLevel.DEBUG) { "PAIRING_REQUEST intent has Bluetooth device with address $androidBtAddressString" }
 
@@ -297,21 +404,45 @@ class AndroidBluetoothInterface(private val androidContext: Context) : Bluetooth
             " Device with address $androidBtAddressString is a Combo pump; accepting Bluetooth pairing request"
         }
 
-        if (!androidBtDevice.setPin(btPairingPin.encodeToByteArray())) {
-            logger(LogLevel.ERROR) { "Could not set Bluetooth pairing PIN" }
-            return
+        // TODO: The following calls can fail, and yet,
+        // the pairing is established successfully.
+        // This requires further clarification.
+
+        try {
+            if (!androidBtDevice.setPin(btPairingPin.encodeToByteArray())) {
+                logger(LogLevel.WARN) { "Could not set Bluetooth pairing PIN" }
+            }
+        } catch (e: Exception) {
+            logger(LogLevel.WARN) { "Caught exception while setting Bluetooth pairing PIN: $e" }
         }
 
-        if (!androidBtDevice.createBond()) {
-            logger(LogLevel.ERROR) { "Could not create bond" }
-            return
+        try {
+            if (!androidBtDevice.createBond()) {
+                logger(LogLevel.WARN) { "Could not create bond" }
+            }
+        } catch (e: Exception) {
+            logger(LogLevel.WARN) { "Caught exception while creating bond: $e" }
         }
 
-        if (!androidBtDevice.setPairingConfirmation(true)) {
-            logger(LogLevel.ERROR) { "Could not set pairing confirmation" }
-            return
+        // TODO: setPairingConfirmation requires the BLUETOOTH_PRIVILEGED
+        // permission. However, this permission is only accessible to
+        // system apps. Leaving this call here in case this can be resolved
+        // somehow in the future, but this most likely throws a
+        // SecurityException on most Android devices.
+        try {
+            if (!androidBtDevice.setPairingConfirmation(true)) {
+                logger(LogLevel.WARN) { "Could not set pairing confirmation" }
+            }
+        } catch (e: Exception) {
+            logger(LogLevel.WARN) { "Caught exception while setting pairing confirmation: $e" }
         }
 
         logger(LogLevel.INFO) { "Established Bluetooth pairing with Combo pump with address $androidBtAddressString" }
+    }
+
+    private fun onDiscoveryFinished() {
+        logger(LogLevel.DEBUG) { "Discovery finished" }
+        // TODO: Is there a way to determine the reason for the stop?
+        discoveryStopped(BluetoothInterface.DiscoveryStoppedReason.MANUALLY_STOPPED)
     }
 }
