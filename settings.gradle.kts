2601f4cfa779dcad1aaea8577701bc784f0858b7
Carlos Rafael Giani <crg7475@mailbox.org>
2021-11-08 12:06:14 +0100

    androidApp: Only build when using Android Studio
    Signed-off-by: Carlos Rafael Giani <crg7475@mailbox.org>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,13 +1,14 @@
 include(":comboctl")
-include(":androidApp")
 
 val env = System.getProperties() ?: mapOf<String, String>()
 
 if (env["idea.platform.prefix"] != "AndroidStudio") {
-    logger.lifecycle("Not building with Android Studio; enabling BlueZ backend and javafxApp")
+    logger.lifecycle("Not building with Android Studio; enabling BlueZ backend and javafxApp, disabling androidApp")
     include(":javafxApp")
     include(":comboctl:src:linuxBlueZCpp")
     include(":comboctl:src:linuxBlueZCpp:external:fmtlib")
     include(":comboctl:src:jvmMain:cpp:linuxBlueZCppJNI")
-} else
-    logger.lifecycle("Building with Android Studio; disabling javafxApp")
+} else {
+    logger.lifecycle("Building with Android Studio; disabling javafxApp, enabling androidApp")
+    include(":androidApp")
+}
