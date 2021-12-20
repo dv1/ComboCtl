330289aec14417739737750a277868bb51d74a6e
Carlos Rafael Giani <crg7475@mailbox.org>
2021-12-20 22:36:40 +0100

    Temporarily disable apps
    This is necessary for the following IO rework. The apps will later
    be re-enabled.
    
    Signed-off-by: Carlos Rafael Giani <crg7475@mailbox.org>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,11 +4,11 @@ val env = System.getProperties() ?: mapOf<String, String>()
 
 if (env["idea.platform.prefix"] != "AndroidStudio") {
     logger.lifecycle("Not building with Android Studio; enabling BlueZ backend and javafxApp, disabling androidApp")
-    include(":javafxApp")
+    //include(":javafxApp")
     include(":comboctl:src:linuxBlueZCpp")
     include(":comboctl:src:linuxBlueZCpp:external:fmtlib")
     include(":comboctl:src:jvmMain:cpp:linuxBlueZCppJNI")
 } else {
     logger.lifecycle("Building with Android Studio; disabling javafxApp, enabling androidApp")
-    include(":androidApp")
+    //include(":androidApp")
 }
