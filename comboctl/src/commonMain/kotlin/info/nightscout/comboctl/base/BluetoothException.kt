ae650378e639d192d616f889cd89ff4478ddba52
Carlos Rafael Giani <crg7475@mailbox.org>
2022-06-24 21:43:58 +0200

    comboctl-base: Add BluetoothPermissionException
    Signed-off-by: Carlos Rafael Giani <crg7475@mailbox.org>

--- comboctl/src/commonMain/kotlin/info/nightscout/comboctl/base/BluetoothException.kt
+++ comboctl/src/commonMain/kotlin/info/nightscout/comboctl/base/BluetoothException.kt
@@ -10,3 +10,16 @@ open class BluetoothException(message: String?, cause: Throwable?) : ComboIOExce
     constructor(message: String) : this(message, null)
     constructor(cause: Throwable) : this(null, cause)
 }
+
+/**
+ * Base class for exceptions that get thrown when permissions for scanning, connecting etc. are missing.
+ *
+ * Subclasses contain OS specific information, like the exact missing permissions.
+ *
+ * @param message The detail message.
+ * @param cause Throwable that further describes the cause of the exception.
+ */
+open class BluetoothPermissionException(message: String?, cause: Throwable?) : BluetoothException(message, cause) {
+    constructor(message: String) : this(message, null)
+    constructor(cause: Throwable) : this(null, cause)
+}
